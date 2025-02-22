6dd7f0658700bb3e5fd7378122f396ab659c54a9
David Soler Recio <daviddsrperiodismo@gmail.com>
2025-02-22 12:49:56 +0100

    Add Jest

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/main.ts',
+    '!src/**/*.module.ts',
+    '!src/**/*.d.ts',
+  ],
+  coverageDirectory: './coverage',
+  testTimeout: 10000,
+};
+
+export default config;
