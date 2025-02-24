3e9965855ec8f10a2cff7f8b5887c9c1a9aba8ca
David Soler Recio <daviddsrperiodismo@gmail.com>
2025-02-24 21:07:51 +0100

    Fix GetActiveContractsByProfileIdUseCase dependency injection

--- src/contracts/application/use-cases/get-active-contracts-by-profile-id.use-case.ts
+++ src/contracts/application/use-cases/get-active-contracts-by-profile-id.use-case.ts
@@ -1,7 +1,9 @@
+import { Inject } from '@nestjs/common';
 import { ContractsRepositoryInterface } from '../../domain/repositories/contracts-repository.interface';
 
 export class GetActiveContractsByProfileIdUseCase {
   constructor(
+    @Inject('ContractsRepositoryInterface')
     private readonly contractsRepository: ContractsRepositoryInterface,
   ) {}
 
